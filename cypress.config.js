83d0076b9ed20b77c0b7cf3eb7b3fcc9f9e49c0e
“rbrletich” <richardbrletich@yahoo.com>
2022-11-25 14:43:58 -0500

    Setup baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   projectId: 'jnp8cq',
   e2e: {
-    setupNodeEvents (on, config) {
-    // implement node event listeners here
-    }
+    baseUrl: 'https://637d2c3e9c2635df8f833d30.mockapi.io'
   }
 })
--- cypress/e2e/ToDoAPI.cy.js
+++ cypress/e2e/ToDoAPI.cy.js
@@ -5,7 +5,7 @@ describe('These tests are used to validate the todo API is functioning as intend
   it('Makes a get call to the todo API and provides parameter \'id\' and \'completed\', then verifies the result', () => {
     const expectedResponse = ApiTestData.idSearchResults
     const parameters = { id: 1 }
-    ApiHelper.makeGetCall('https://637d2c3e9c2635df8f833d30.mockapi.io/todos', parameters)
+    ApiHelper.makeGetCall('/todos', parameters)
       .then(({ status, body }) => {
         ApiHelper.makeTodoGetAssertions(status, body, expectedResponse)
       })
@@ -14,7 +14,7 @@ describe('These tests are used to validate the todo API is functioning as intend
   it('Makes a call to the todo API and provides parameter \'completed\', then verifies the result', () => {
     const expectedResponse = ApiTestData.completedSearchResults
     const parameters = { completed: true }
-    ApiHelper.makeGetCall('https://637d2c3e9c2635df8f833d30.mockapi.io/todos', parameters)
+    ApiHelper.makeGetCall('/todos', parameters)
       .then(({ status, body }) => {
         ApiHelper.makeTodoGetAssertions(status, body, expectedResponse)
       })
@@ -23,13 +23,13 @@ describe('These tests are used to validate the todo API is functioning as intend
   it('Makes a post call to create a todo record. Then, makes a get call to verify the record has the correct data before deleting the record', () => {
     const createTodoBody = ApiTestData.createTodoBody
     cy.log(createTodoBody)
-    let options = { url: 'https://637d2c3e9c2635df8f833d30.mockapi.io/todos', body: createTodoBody, method: 'post' }
+    let options = { url: '/todos', body: createTodoBody, method: 'post' }
     // Makes a put call to create a data entry with known data.
     ApiHelper.makeApiCall(options)
       .then(({ status, body }) => {
         const id = body.id // Getting the created Todo id value to perform a get call
         const parameters = { id: id } // 
-        options = { url: 'https://637d2c3e9c2635df8f833d30.mockapi.io/todos', parameters, method: 'get' }
+        options = { url: '/todos', parameters, method: 'get' }
       })
       .then(({ status, body }) => {
         ApiHelper.makeApiCall(options)
@@ -39,7 +39,7 @@ describe('These tests are used to validate the todo API is functioning as intend
           })
         const id = body.id
         const parameters = { id: id }
-        options = { url: 'https://637d2c3e9c2635df8f833d30.mockapi.io/todos', id, method: 'delete' }
+        options = { url: '/todos', id, method: 'delete' }
       })
       .then(() => {
         ApiHelper.makeApiCall(options)
